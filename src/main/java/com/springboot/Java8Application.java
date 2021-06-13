f691ddc95703f5586e5fc7757069555aa12595c1
yhzRoot <68263057+yhzRoot@users.noreply.github.com>
2021-06-13 19:03:07 +0800

    Create Java8Application.java
    修改

--- src/main/java/com/springboot/Java8Application.java
+++ src/main/java/com/springboot/Java8Application.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Java8Application {
 
-    public static void main(String[] args) {
+    public static void main(String[ ] args) {
         SpringApplication.run(Java8Application.class, args);
     }
 
